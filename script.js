a3887ea68f5d61a667156724cb8afe2e8b0a78f1
Zentaa1 <117643567+Zentaa1@users.noreply.github.com>
2023-02-19 21:50:34 +0100

    1.0

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("Active")
     navMenu.classList.toggle("Active")
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+    })) 
